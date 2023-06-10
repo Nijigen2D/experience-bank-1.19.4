d276812fbfb10bf9f63fdc675c85cbfea924f9ec
Nijigen2D <ajgop02@gmail.com>
2023-06-09 18:52:50 -0700

    Initial commit

---
+++ src/main/java/net/nijigen/ExperienceBankClient.java
@@ -0,0 +1,10 @@
+package net.nijigen;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ExperienceBankClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
